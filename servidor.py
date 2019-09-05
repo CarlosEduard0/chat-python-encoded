38c2c23e1ce2a79a5e001829ef49b41ca199d947
Carlos Eduardo <carloseduardofox8@gmail.com>
2019-09-05 18:02:20 -0300

    Adiciona servidor

---
+++ servidor.py
@@ -0,0 +1,30 @@
+from socket import socket, AF_INET, SOCK_STREAM
+import threading
+
+def escutarCliente(con):
+    while True:
+        msg = con.recv(1024)
+        if not msg: break
+        print(str(msg, 'utf-8'))
+
+tcp = socket(AF_INET, SOCK_STREAM)
+tcp.bind(('127.0.0.1', 5047))
+tcp.listen(1)
+
+print('Servidor iniciado')
+print('Aguardando cliente')
+
+conexao, cliente = tcp.accept()
+print('Cliente {} conectado'.format(cliente))
+
+threadEscutar = threading.Thread(target = escutarCliente, args = (conexao,))
+threadEscutar.start()
+
+mensagem = input()
+while True:
+    conexao.send(mensagem.encode())
+    mensagem = input()
+
+threadEscutar.join()
+tcp.close()
+exit()
